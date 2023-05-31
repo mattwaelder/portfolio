fde4ccf2a4fac7e60120a565501edfc6af1b24bc
Matt Waelder <mattwaelder@gmail.com>
2023-05-31 16:36:54 -0700

    css styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,15 +3,16 @@ import "./Navbar.scss";
 
 const Navbar = () => {
   return (
-    <div className="App__nav-container">
-      <h2>Matt</h2>
+    <nav className="App__nav-container">
+      <h2>MattLogo</h2>
       <ul className="App__nav-list">
-        <li>About</li>
-        <li>Languages</li>
-        <li>projects</li>
-        <li>contact</li>
+        {["home", "about", "skills", "contact"].map((el) => (
+          <li>
+            <a href={`#${el}`}>{el}</a>
+          </li>
+        ))}
       </ul>
-    </div>
+    </nav>
   );
 };
 export default Navbar;
